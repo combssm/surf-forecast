a26d4557ae2f08decd6c5d4d1df8382466ae5247
seanc <seanc@inmotionhosting.co>
2020-01-08 16:34:55 -0500

    jan062020: table style output

--- forecast.py
+++ forecast.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import requests
 import time
+import plotly.graph_objects as go
 
 app = Flask(__name__)
 
@@ -22,7 +23,7 @@ class Forecast:
 def home():
     """Displays main page"""
     try:
-        response = requests.get('http://magicseaweed.com/api/e53638829bea94ae3a45213abb63a7ad/forecast/?spot_id=398').json()
+        response = requests.get('http://magicseaweed.com/api/e53638829bea94ae3a45213abb63a7ad/forecast/?spot_id=398&units=us').json()
     except Exception as e:
         return "<h2>Unable to retrieve swell data: {}</h2>".format(e)
 
@@ -30,7 +31,11 @@ def home():
     for i in response:
         f = Forecast(i)
         fdata.append(f)
-    return render_template('index.html', fdata=fdata)
+    graph = go.Figure(
+        data=[go.Bar(x=[x.timestamp for x in fdata],y=[y.max_swell_height for y in fdata])],
+        layout_title_text="4 day Forecast for S-Turns, NC"
+    )
+    return render_template('index.html', fdata=fdata, graph=graph)
 
 if __name__ == '__main__':
     app.run(debug=True)
