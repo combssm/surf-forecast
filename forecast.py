251beb938743508519f381939f8d5e178f45d694
seanc <seanc@inmotionhosting.co>
2020-01-08 10:56:30 -0500

    jan062020: table style output

--- forecast.py
+++ forecast.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template
 from plotly.offline import plot
 from plotly.graph_objs import Scatter
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     """Displays main page"""
-    my_plot_div = plot([Scatter(x=[1, 2, 3], y=[3, 1, 6])], output_type='div')
-    return render_template('index.html', my_plot_div=my_plot_div)
+    try:
+        response = requests.get('http://magicseaweed.com/api/e53638829bea94ae3a45213abb63a7ad/forecast/?spot_id=398').json()
+    except Exception as e:
+        return "<h2>Unable to retrieve swell data: {}</h2>".format(e)
+
+    item = response[0]
+    return render_template('index.html', item=item)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
