8c595663b65ef4cfb6320174e7ab4f74337c9065
seanc <seanc@inmotionhosting.co>
2020-01-22 12:00:48 -0500

    jan202020: collapse

--- static/scripts.js
+++ static/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('[data-toggle="toggle"]').change(function(){
-		$(this).parents().next('.hide').toggle();
-	});
+    $('[data-toggle="toggle"]').change(function(){
+        $(this).parents().next('.hide').toggle();
+    });
 });
