c6c6b26c2cadabc722d476850d474bd30c439fac
seanc <seanc@inmotionhosting.co>
2020-02-25 11:50:52 -0500

    feb192020: migration to dash framework

--- forecast.py
+++ forecast.py
@@ -4,9 +4,9 @@ import dash_html_components as html
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+forecast = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div(children=[
+forecast.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
 
     html.Div(children='''
@@ -28,4 +28,4 @@ app.layout = html.Div(children=[
 ])
 
 if __name__ == '__main__':
-    app.run_server()
+    forecast.run_server()
