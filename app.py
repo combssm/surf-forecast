65d482b6ecff06dca0d94ec3f8c355925084d5f4
Sean <seanc@inmotionhosting.com>
2020-03-05 16:07:02 -0500

    feb192020: hoverdata:

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import requests
 import plotly.graph_objs as go
 import time
 from datetime import datetime
+import json
 
 API_KEY = 'e53638829bea94ae3a45213abb63a7ad'
 FIELDS = [
@@ -35,19 +37,31 @@ app.layout = html.Div([
                 go.Scatter(
                     x = [datetime.fromtimestamp(f['timestamp']) for f in response],
                     y = [f['swell']['components']['primary']['height'] for f in response],
+                    customdata=[{
+                        'temp': t['condition']['temperature'],
+                        'solidRating': t['solidRating'],
+                        'wind_speed': t['wind']['speed'],
+                        'wind_direction': t['wind']['direction'],
+                        'wind_compass_direction': t['wind']['compassDirection']}
+                         for t in response],
                     mode = 'lines+markers',
                     name = 'primary'
                 )
             ],
             'layout': go.Layout(
-                title = 'Surf Forecast',
+                title = '5 Day Virginia Beach Surf Forecast',
                 xaxis = {'title': 'Date'},
-                yaxis = {'title': 'Wave Height'},
+                yaxis = {'title': 'Wave Height (ft)'},
                 hovermode='closest'
             )
         }
-    )
+    ),
+    html.Pre(id='hover-data', style={'paddingTop':35})
 ])
+
+@app.callback(Output('hover-data', 'children'), [Input('surf-forecast', 'hoverData')])
+def update_hoverdata(hoverData):
+    return json.dumps(hoverData, indent=2)
 
 if __name__ == '__main__':
     app.run_server()
