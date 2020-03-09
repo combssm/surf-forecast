b70d7b56868027ffd0bb10838349b64aa77756c4
seanc <seanc@inmotionhosting.co>
2020-03-09 10:28:07 -0400

    mar072020: dropdown spot selector

--- app.py
+++ app.py
@@ -24,21 +24,22 @@ FIELDS = [
 app = dash.Dash()
 server = app.server
 
-try:
+app.config['suppress_callback_exceptions'] = True
+
+app.layout = html.Div([
+    dcc.Dropdown(id='spot-picker', options=[{'label': str('Virginia Beach'), 'value': 396}, {'label': 'S-Turns', 'value': 398}], value='Virginia Beach', placeholder="Choose Spot"),
+    dcc.Graph(id='forecast-graph')
+], style={'align': 'center'})
+
+@app.callback(Output('forecast-graph', 'figure'), [Input('spot-picker', 'value')])
+def update_figure(selected_spot):
     response = requests.get(
         'http://magicseaweed.com/api/{}/forecast/?spot_id={}&units={}&fields={}'.format(
             API_KEY,
-            "396", # Virginia Beach
+            selected_spot,
             "us", 
-            ','.join(FIELDS))
-        ).json()
-except Exception as e:
-    print("<h2>Unable to retrieve swell data: {}</h2>".format(e))
-
-app.layout = html.Div([
-    dcc.Graph(
-        id='surf-forecast',
-        figure={
+            ','.join(FIELDS))).json()
+    graph = {
             'data': [
                 # primary
                 go.Scatter(
@@ -66,96 +67,12 @@ app.layout = html.Div([
                 )
             ],
             'layout': go.Layout(
-                title = '5 Day Virginia Beach Surf Forecast',
+                title = '5 Day Surf Forecast',
                 yaxis = {'title': 'Wave Height (ft)'},
                 hovermode='closest',
-                #plot_bgcolor='aliceblue',
-                #paper_bgcolor='#0079c4'
             )
-        }
-    ),
-    html.Div(id='forecast-table'),
-    html.Pre(id='hover-data', style={'paddingTop':35})
-], style={'align': 'center'})
-
-
-@app.callback(Output('forecast-table', 'children'), [Input('surf-forecast', 'hoverData')])
-def update_forecast_table(hoverData):
-    if not hoverData:
-        return "Hover over a point"
-
-    # Generate swell size format string
-    min = hoverData['points'][0]['customdata']['swell']['minBreakingHeight']
-    max = hoverData['points'][0]['customdata']['swell']['maxBreakingHeight']
-    if min == max:
-        swell_size = "{}ft".format(min)
-    else:
-        swell_size = "{}-{}ft".format(min, max)
-
-    # Generate wind speed format string
-    speed = hoverData['points'][0]['customdata']['wind']['speed']
-    direction = hoverData['points'][0]['customdata']['wind']['compassDirection']
-    wind = "{}mph {}".format(speed, direction)
-
-    # Generate swell stats
-    primary_swell = str(hoverData['points'][0]['customdata']['swell']['components']['primary']['height']) + " " + \
-                str(hoverData['points'][0]['customdata']['swell']['components']['primary']['compassDirection']) + " @ " + \
-                str(hoverData['points'][0]['customdata']['swell']['components']['primary']['period']) + "s"
-
-    try:
-        secondary_swell = str(hoverData['points'][0]['customdata']['swell']['components']['secondary']['height']) + " " + \
-                        str(hoverData['points'][0]['customdata']['swell']['components']['secondary']['compassDirection']) + " @ " + \
-                        str(hoverData['points'][0]['customdata']['swell']['components']['secondary']['period']) + "s"
-    except Exception:
-        secondary_swell = ""
-    try:
-        tertiary_swell = str(hoverData['points'][0]['customdata']['swell']['components']['tertiary']['height']) + " " + \
-                        str(hoverData['points'][0]['customdata']['swell']['components']['tertiary']['compassDirection']) + " @ " + \
-                        str(hoverData['points'][0]['customdata']['swell']['components']['tertiary']['period']) + "s"                
-    except Exception:
-        tertiary_swell = ""
-
-    table = html.Table([
-        # Date Header
-        html.Thead(
-            datetime.strftime(
-                datetime.fromtimestamp(
-                    hoverData['points'][0]['customdata']['timestamp']), 
-                    "%B %d, %Y %I:%M%p"
-                    ), style={'textAlign': 'center'}
-                ),
-        # Column Headers
-        html.Tr([
-            html.Td("Swell Data"),
-            html.Td("Tide Data"),
-            html.Td("Weather Data")
-        ]),
-        html.Tr([
-            html.Td("Swell Size: {}".format(swell_size)),
-            html.Td("High Tides: {}/{}".format("TBD", "TBD")),
-            html.Td("Wind: {}".format(wind))
-        ]),
-        html.Tr([
-            html.Td("Primary Swell: {}".format(primary_swell)),
-            html.Td("Low Tides: {}/{}".format("TBD", "TBD")),
-            html.Td("Temperature: {}".format(hoverData['points'][0]['customdata']['condition']['temperature']))
-        ]),
-        html.Tr([
-            html.Td("Secondary Swell: {}".format(secondary_swell)),
-            html.Td(""),
-            html.Td("Feels Like: {}".format(hoverData['points'][0]['customdata']['wind']['chill']))
-        ]),
-        html.Tr([
-            html.Td("Tertiary Swell: {}".format(tertiary_swell)),
-            html.Td(""),
-            html.Td("Weather: {}".format(hoverData['points'][0]['customdata']['condition']['weather']))
-        ])
-    ], style={'border': '1px solid black'})
-    return table
-
-@app.callback(Output('hover-data', 'children'), [Input('surf-forecast', 'hoverData')])
-def update_hoverdata(hoverData):
-    return json.dumps(hoverData, indent=2)
+    }
+    return graph
 
 if __name__ == '__main__':
     app.run_server()
