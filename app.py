20074cf443003c17712c1b6bf28ea32022f0085a
seanc <seanc@inmotionhosting.co>
2020-03-16 09:45:22 -0400

    mar072020: trying to fix 404s

--- app.py
+++ app.py
@@ -14,81 +14,38 @@ import pandas as pd
 
 
 API_KEY = 'e53638829bea94ae3a45213abb63a7ad'
-FIELDS = ['timestamp', 'swell.components.*']
+FIELDS = ['timestamp', 'swell.components.primary.height']
 SPOTS = [{'label': str('Virginia Beach'), 'value': 396},
          {'label': str('S-Turns'), 'value': 398}]
 
-app = dash.Dash(serve_locally = False)
+app = dash.Dash()
 server = app.server
 
-app.layout = html.Div([
-    html.Div([
-        dcc.Dropdown(
-            id='spot-picker',
-            options=[{'label': i['label'], 'value': i['value']} for i in SPOTS],
-            value=396,
-            clearable=False,
-            style={'border': '3px solid orange'}
-        )
-    ], style={'border': '3px solid green'}),
-    html.Div([
-        dcc.Graph(
-            id='forecast-graph',
-            config={'displayModeBar': False},
-        )
-    ], style={'border': '3px solid green'}),
-    html.Pre("Forecast Details",
-        id='forecast-detail',
-        style={'border': '3px solid red'}),   
-], style={'width': '50%', 'margin': 'auto', 'padding': '20px', 'border': '3px solid blue'})
+response = requests.get(
+    'http://magicseaweed.com/api/{}/forecast/?spot_id={}&units={}&fields={}'.format(
+        API_KEY,
+        396,
+        "us", 
+        ','.join(FIELDS))).json()
 
-
-@app.callback(Output('forecast-graph', 'figure'),[Input('spot-picker', 'value')])
-def update_figure(selected_spot):
-    response = requests.get(
-        'http://magicseaweed.com/api/{}/forecast/?spot_id={}&units={}&fields={}'.format(
-            API_KEY,
-            selected_spot,
-            "us", 
-            ','.join(FIELDS))).json()
-    graph = {
-            'data': [
-                # primary
-                go.Scatter(
-                    x = [datetime.fromtimestamp(f['timestamp']) for f in response],
-                    y = [f['swell']['components']['primary']['height'] if f['swell']['components'].get('primary') else 'null' for f in response],
-                    customdata=response,
-                    mode = 'lines+markers',
-                    name = 'primary'
-                ),
-                # secondary
-                go.Scatter(
-                    x = [datetime.fromtimestamp(f['timestamp']) for f in response],
-                    y = [f['swell']['components']['secondary']['height'] if f['swell']['components'].get('secondary') else 'null' for f in response],
-                    customdata=response,
-                    mode = 'lines+markers',
-                    name = 'secondary'
-                ),
-                # tertiary
-                go.Scatter(
-                    x = [datetime.fromtimestamp(f['timestamp']) for f in response],
-                    y = [f['swell']['components']['tertiary']['height'] if f['swell']['components'].get('tertiary') else 'null' for f in response],
-                    customdata=response,
-                    mode = 'lines+markers',
-                    name = 'tertiary'
-                )
-            ],
-            'layout': go.Layout(
-                title = '5 Day Surf Forecast',
-                yaxis = {'title': 'Wave Height (ft)'},
-                hovermode='closest',
+figure = {
+        'data': [
+            # primary
+            go.Scatter(
+                x = [datetime.fromtimestamp(f['timestamp']) for f in response],
+                y = [f['swell']['components']['primary']['height'] for f in response],
+                mode = 'lines+markers',
+                name = 'primary'
             )
-    }
-    return graph
+        ],
+        'layout': go.Layout(
+            title = '5 Day Surf Forecast',
+            yaxis = {'title': 'Wave Height (ft)'},
+            hovermode='closest',
+        )
+}
 
-@app.callback(Output('forecast-detail','children'), [Input('forecast-graph', 'clickData')])
-def update_forecast_detail(click_data):
-    return json.dumps(click_data,indent=2)
+app.layout = html.Div([dcc.Graph(id='forecast-graph', figure=figure)])
 
 if __name__ == '__main__':
     app.run_server()
