5cb397eeb7dd554b238b5b9f0d498cb488f57bf4
Sean Combs <combssm@gmail.com>
2020-03-21 12:12:15 -0400

    woah

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import plotly.graph_objs as go
 
 
 API_KEY = 'e53638829bea94ae3a45213abb63a7ad'
-FIELDS = ['timestamp', 'swell.components.primary.height', 'swell.components.secondary.height', 'swell.components.tertiary.height']
+FIELDS = ['timestamp', 'swell.*', 'wind.*', 'condition.temperature']
 SPOTS = [{'label': str('Virginia Beach'), 'value': 396},
          {'label': str('S-Turns'), 'value': 398}]
 
@@ -33,21 +33,24 @@ figure = {
                 x = [datetime.fromtimestamp(f['timestamp']) for f in response],
                 y = [f['swell']['components']['primary']['height'] for f in response],
                 mode = 'lines+markers',
-                name = 'primary'
+                name = 'primary',
+                customdata=response
             ),
             # secondary
             go.Scatter(
                 x = [datetime.fromtimestamp(f['timestamp']) for f in response],
                 y = [f['swell']['components']['secondary']['height'] if f['swell']['components'].get('secondary') else 'asdfds' for f in response],
                 mode = 'lines+markers',
-                name = 'secondary'
+                name = 'secondary',
+                customdata=response
             ),
             # tertiary
             go.Scatter(
                 x = [datetime.fromtimestamp(f['timestamp']) for f in response],
                 y = [f['swell']['components']['tertiary']['height'] if f['swell']['components'].get('tertiary') else 'sdfdsf' for f in response],
                 mode = 'lines+markers',
-                name = 'tertiary'
+                name = 'tertiary',
+                customdata=response
             )
         ],
         'layout': go.Layout(
@@ -63,6 +66,18 @@ app.layout = html.Div(
         html.Div("Some Text", id='forecast-detail', style={'border': '3px solid black', 'float': 'left', 'width': '24%'})
     ], style={'display': 'inline-block', 'padding': '15px', 'width': '100%', 'border': '3px solid black'}
 )
+
+@app.callback(Output('forecast-detail', 'children'), [Input('forecast-graph', 'hoverData')])
+def update_forecast_detail(hoverData):
+    return html.Pre("""Forecast Details:
+Date:            {datetime.fromtimestamp(hoverData['timestamp'])}
+Primary Swell:   {hoverData['swell']['components']['primary']['height']} @ {hoverData['swell']['components']['primary']['period']}s {hoverData['swell']['components']['primary']['compassDirection']}
+Secondary Swell: {hoverData['swell']['components']['secondary']['height']} @ {hoverData['swell']['components']['secondary']['period']}s {hoverData['swell']['components']['secondary']['compassDirection']}
+Tertiary Swell:  {hoverData['swell']['components']['tertiary']['height']} @ {hoverData['swell']['components']['tertiary']['period']}s {hoverData['swell']['components']['tertiary']['compassDirection']}
+Wind Condition:  {hoverData['wind']['speed']}mph {hoverData['wind']['compassDirection']}
+Temperature:     {hoverdata['condition']['temperature']}F""".format(**hoverData)
+    )
+
 
 if __name__ == '__main__':
     app.run_server()
