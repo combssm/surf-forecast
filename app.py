af5c5c9369d0f16d3417007891cd5da22c315dd2
Sean Combs <combssm@gmail.com>
2020-03-22 08:27:47 -0400

    html title

--- app.py
+++ app.py
@@ -76,6 +76,7 @@ figure = {
 
 app.layout = html.Div(
     [
+        html.Title("Combsvb Surf Forecast"),
         html.Div(dcc.Graph(id='forecast-graph', figure=figure), style={'border': '3px solid black', 'float': 'left', 'width': '72%', 'margin': '5px'}),
         html.Div("Click on a point to see more details", id='forecast-detail', style={'border': '3px solid black', 'float': 'left', 'width': '20%', 'padding': '10px', 'margin': '5px'})
     ], style={'display': 'inline-block', 'padding': '15px', 'width': '90%'}
