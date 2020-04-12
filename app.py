6116c486598a2d61410a9401ed190c69ea311d21
Sean Combs <combssm@gmail.com>
2020-04-12 11:51:57 -0400

    live updates

--- app.py
+++ app.py
@@ -20,70 +20,77 @@ app = dash.Dash()
 server = app.server
 app.title = 'Combsvb Surf Forecast'
 
-response = requests.get(
-    'http://magicseaweed.com/api/{}/forecast/?spot_id={}&units={}&fields={}'.format(
-        API_KEY,
-        396,
-        "us", 
-        ','.join(FIELDS))).json()
+def serve_layout():
 
-figure = {
-        'data': [
-            # primary
-            go.Scatter(
-                x = [datetime.fromtimestamp(f['timestamp']) for f in response],
-                y = [f['swell']['components']['primary']['height'] for f in response],
-                mode = 'lines+markers',
-                name = 'primary',
-                customdata=response
-            ),
-            # secondary
-            go.Scatter(
-                x = [datetime.fromtimestamp(f['timestamp']) for f in response],
-                y = [f['swell']['components']['secondary']['height'] if f['swell']['components'].get('secondary') else 'asdfds' for f in response],
-                mode = 'lines+markers',
-                name = 'secondary',
-                customdata=response
-            ),
-            # tertiary
-            go.Scatter(
-                x = [datetime.fromtimestamp(f['timestamp']) for f in response],
-                y = [f['swell']['components']['tertiary']['height'] if f['swell']['components'].get('tertiary') else 'sdfdsf' for f in response],
-                mode = 'lines+markers',
-                name = 'tertiary',
-                customdata=response
-            ),
-            # swell.minBreakingHeight
-            go.Bar(
-                x = [datetime.fromtimestamp(f['timestamp']) for f in response],
-                y = [f['swell']['minBreakingHeight'] if f['swell'].get('minBreakingHeight') else 0 for f in response],
-                name = 'Min. Breaking Height',
-                marker = dict(color='#9EA0A1')
-            ),
-            # swell.maxBreakingHeight
-            go.Bar(
-                x = [datetime.fromtimestamp(f['timestamp']) for f in response],
-                y = [f['swell']['maxBreakingHeight'] if f['swell'].get('maxBreakingHeight') else 0 for f in response],
-                name = 'Min. Breaking Height',
-                marker = dict(color='#FFD700')
-            )            
-        ],
-        'layout': go.Layout(
-            title = '5 Day Surf Forecast',
-            yaxis = {'title': 'Wave Height (ft)'},
-            hovermode='closest',
-        )
-}
+    response = requests.get(
+        'http://magicseaweed.com/api/{}/forecast/?spot_id={}&units={}&fields={}'.format(
+            API_KEY,
+            396,
+            "us", 
+            ','.join(FIELDS))).json()
 
-app.layout = html.Div(
-    [
-        html.Div(dcc.Graph(id='forecast-graph', figure=figure), style={'border': '3px solid black', 'float': 'left', 'width': '70%', 'margin': '5px'}),
-        html.Div("Click on a point to see more details", id='forecast-detail', style={'border': '3px solid black', 'float': 'left', 'width': 'auto', 'padding': '10px', 'margin': '5px'})
-    ], style={'display': 'inline-block', 'padding': '15px', 'width': '100%', 'position': 'fixed'}
-)
+    figure = {
+            'data': [
+                # primary
+                go.Scatter(
+                    x = [datetime.fromtimestamp(f['timestamp']) for f in response],
+                    y = [f['swell']['components']['primary']['height'] for f in response],
+                    mode = 'lines+markers',
+                    name = 'primary',
+                    customdata=response
+                ),
+                # secondary
+                go.Scatter(
+                    x = [datetime.fromtimestamp(f['timestamp']) for f in response],
+                    y = [f['swell']['components']['secondary']['height'] if f['swell']['components'].get('secondary') else 'asdfds' for f in response],
+                    mode = 'lines+markers',
+                    name = 'secondary',
+                    customdata=response
+                ),
+                # tertiary
+                go.Scatter(
+                    x = [datetime.fromtimestamp(f['timestamp']) for f in response],
+                    y = [f['swell']['components']['tertiary']['height'] if f['swell']['components'].get('tertiary') else 'sdfdsf' for f in response],
+                    mode = 'lines+markers',
+                    name = 'tertiary',
+                    customdata=response
+                ),
+                # swell.minBreakingHeight
+                go.Bar(
+                    x = [datetime.fromtimestamp(f['timestamp']) for f in response],
+                    y = [f['swell']['minBreakingHeight'] if f['swell'].get('minBreakingHeight') else 0 for f in response],
+                    name = 'Min. Breaking Height',
+                    marker = dict(color='#9EA0A1')
+                ),
+                # swell.maxBreakingHeight
+                go.Bar(
+                    x = [datetime.fromtimestamp(f['timestamp']) for f in response],
+                    y = [f['swell']['maxBreakingHeight'] if f['swell'].get('maxBreakingHeight') else 0 for f in response],
+                    name = 'Min. Breaking Height',
+                    marker = dict(color='#FFD700')
+                )            
+            ],
+            'layout': go.Layout(
+                title = '5 Day Surf Forecast',
+                yaxis = {'title': 'Wave Height (ft)'},
+                hovermode='closest',
+            )
+    }
+
+    return html.Div(
+        [
+            html.Div(dcc.Graph(id='forecast-graph', figure=figure), style={'border': '3px solid black', 'float': 'left', 'width': '70%', 'margin': '5px'}),
+            html.Div("Click on a point to see more details", id='forecast-detail', style={'border': '3px solid black', 'float': 'left', 'width': 'auto', 'padding': '10px', 'margin': '5px'})
+        ], style={'display': 'inline-block', 'padding': '15px', 'width': '100%', 'position': 'fixed'}
+    )
+
+#https://dash.plotly.com/live-updates
+app.layout = serve_layout
 
 @app.callback(Output('forecast-detail', 'children'), [Input('forecast-graph', 'clickData')])
 def update_forecast_detail(clickData):
+    if not clickData:
+        return "Click on a point to see more details"
     date = datetime.fromtimestamp(clickData['points'][0]['customdata']['timestamp'])
     if clickData['points'][0]['customdata']['swell']['minBreakingHeight'] == clickData['points'][0]['customdata']['swell']['maxBreakingHeight']:
         swell_size =      "Swell Size      : {}".format(clickData['points'][0]['customdata']['swell']['minBreakingHeight'])
